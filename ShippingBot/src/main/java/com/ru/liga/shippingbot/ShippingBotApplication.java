cdd0d9a0e2c1c6a6d723e18217949dd57777f872
Knoxa <216345i@mail.ru>
2022-04-05 16:22:25 +0300

    Initial commit

---
+++ ShippingBot/src/main/java/com/ru/liga/shippingbot/ShippingBotApplication.java
@@ -0,0 +1,13 @@
+package com.ru.liga.shippingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingBotApplication.class, args);
+    }
+
+}
